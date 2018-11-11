a3f7f73f103537c86654ef5626df4747e3761f4e
nidhal-ferjani <nidhal.ferjani@gmail.com>
2018-11-11 19:04:57 +0100

    add suaceLabs configuration

--- src/test/java/com/selenium/taf/data/DataProperties.java
+++ src/test/java/com/selenium/taf/data/DataProperties.java
@@ -24,4 +24,6 @@ public class DataProperties {
 		
 		return dataProperties;
 	}
+	
+	
 }
--- src/test/java/com/selenium/taf/tests/TestBaseGrid.java
+++ src/test/java/com/selenium/taf/tests/TestBaseGrid.java
@@ -2,6 +2,7 @@ package com.selenium.taf.tests;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Properties;
 
 import org.junit.AfterClass;
 import org.openqa.selenium.WebDriver;
@@ -14,14 +15,26 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
+import com.selenium.taf.data.DataProperties;
 import com.selenium.taf.utilities.Helper;
 
 public class TestBaseGrid {
+	
+	// Sauce Labs Configuration
+	
+	private static Properties userDataSauce = DataProperties.loadProperties(System.getProperty("user.dir")
+			                                +"\\src\\main\\java\\com\\selenium\\taf\\properties\\saucelabsUser.properties");
+	
+	private static final String USERNAME = userDataSauce.getProperty("username");
+	private static final String ACCESS_KEY = userDataSauce.getProperty("accesskey");
+	private static final String SAUCE_URL = "http://"+USERNAME+":"+ACCESS_KEY
+			                                 +userDataSauce.getProperty("seleniumURL");
+	
 	protected static ThreadLocal<RemoteWebDriver> webDriver;
 
 	@BeforeClass
-	@Parameters({ "browser", "baseURL" })
-	public void startDriver(@Optional("chrome") String browser, @Optional("https://google.com") String URL)
+	@Parameters({ "browser", "baseURL", "platform" })
+	public void startDriver(@Optional("chrome") String browser, @Optional("https://google.com") String URL, String platform)
 			throws MalformedURLException {
 		Reporter.log("TestBase ----> startDriver", true);
 		webDriver = new ThreadLocal<>();
@@ -31,9 +44,16 @@ public class TestBaseGrid {
 
 		// Set BrowserName
 		capabilities.setCapability("browserName", browser);
+		
+		// Set Platform
+		//capabilities.setCapability("platform", platform);
+		
+		// Set Browser to : Grid SAUCE LABS
+		webDriver.set(new RemoteWebDriver(new URL(SAUCE_URL), capabilities));
+		
 
-		// Set Browser to ThreadLocalMap
-		webDriver.set(new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities));
+		// Set Browser to ThreadLocalMap : Grid Local
+		//webDriver.set(new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities));
 
 		getDriver().get(URL);
 	}
